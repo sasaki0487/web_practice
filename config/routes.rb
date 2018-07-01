ee05c62c4f9d261c0f18dd82b25472c389d74fa8
sasaki5221 <bobbytsai040@gmail.com>
2018-07-01 23:06:59 +0800

    test

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'home/index'
-  resources :articles
- 
-  root 'home#index'
+  get 'home/index', to: 'home#index'
+  get 'home/momo-gen', to: 'home#momo_gen'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
